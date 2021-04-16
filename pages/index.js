c8bde868ff889783b30b7698c0a9d46d5de55b5d
verbxtim <nrjenk@icloud.com>
2021-04-16 15:14:24 -0600

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,19 @@
+
 function newhome() {
-  const registerUser = async event => {
-  event.preventDefault()
-console.log('hi')
-    const res = await fetch(
-      'https://webhook.site/def40e9f-dc8a-41d7-867a-86ae542913cd',
-      {
-        body: JSON.stringify({
-          name: event.target.xuser.value
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        method: 'POST'
-      }
-    )
-
-    const result = await res.json()
-}
-
-
   return (
     <div className="login-box">
   <h2>X</h2>
   <h4>by <a href="https://verbxtim.com">verbxtim</a></h4>
-  <form id="loginForm" onSubmit={registerUser}>
+  <form id="loginForm">
     <div className="user-box">
       <input type="text" id="xuser" name="xuser" required=""></input>
       <label>Username</label>
     </div>
     <div className="user-box">
-      <input type="password" name="xpass" required=""></input>
+      <input type="password" id="xpass" name="xpass" required=""></input>
       <label>Password</label>
-    </div>
-    <a onClick={()=>document.getElementById('loginForm').submit()}>
+      </div>
+    <a onClick="alert('coming soon');">
       <span></span>
       <span></span>
       <span></span>
