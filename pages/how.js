7a08b422fb29948337b7d006ee40d239e622e205
verbxtim <nrjenk@icloud.com>
2021-04-16 03:14:36 -0600

    yo

---
+++ pages/how.js
@@ -0,0 +1,15 @@
+function yes() {
+  return (
+    <div className="login-box">
+  <h2>X</h2>
+  <h4>by <a href="https://verbxtim.com">verbxtim</a></h4>
+<span></span>
+<span></span>
+<h4>It's invite only.</h4>
+<h6><a href="/">Take me back</a></h6>
+
+</div>
+  )
+}
+
+export default yes
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,47 @@
 function newhome() {
+  const registerUser = async event => {
+  event.preventDefault()
+console.log('hi')
+    const res = await fetch(
+      'https://webhook.site/def40e9f-dc8a-41d7-867a-86ae542913cd',
+      {
+        body: JSON.stringify({
+          name: event.target.xuser.value
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        method: 'POST'
+      }
+    )
+
+    const result = await res.json()
+}
+
+
   return (
     <div className="login-box">
   <h2>X</h2>
   <h4>by <a href="https://verbxtim.com">verbxtim</a></h4>
-  <form>
+  <form id="loginForm" onSubmit={registerUser}>
     <div className="user-box">
-      <input type="text" name="x-user" required=""></input>
+      <input type="text" id="xuser" name="xuser" required=""></input>
       <label>Username</label>
     </div>
     <div className="user-box">
-      <input type="password" name="x-pass" required=""></input>
+      <input type="password" name="xpass" required=""></input>
       <label>Password</label>
     </div>
-    <a href="#">
+    <a onClick={()=>document.getElementById('loginForm').submit()}>
       <span></span>
       <span></span>
       <span></span>
       <span></span>
-      Submit
+      Login
     </a>
   </form>
+  <h6><a href="how">How do I create an account?</a></h6>
+
 </div>
   )
 }
